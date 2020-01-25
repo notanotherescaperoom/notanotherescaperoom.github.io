1e29b2aa5288876b8b77980615df304b60e54826
notanotherescaperoom <55063066+notanotherescaperoom@users.noreply.github.com>
2020-01-25 12:56:23 -0800

    9-10-19

--- js/changes.js
+++ js/changes.js
@@ -1,13 +1,3 @@
-$(document).bind('scroll',function(e){
-    $('h1').each(function(){
-        if (
-           $(this).offset().top < window.pageYOffset + 10
-//begins before top
-        && $(this).offset().top + $(this).height() > window.pageYOffset + 10
-//but ends in visible area
-//+ 10 allows you to change hash before it hits the top border
-        ) {
-            window.location.hash = $(this).attr('id');
-        }
-    });
+$("a.nav-link").click(function() {
+   $(".navbar-toggler").dropdown("toggle");
 });
